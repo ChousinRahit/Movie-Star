a4da724bdd0ad3ab20b1fe693b7b7603e80de3ba
ChousinRahit <2428kcr@gmail.com>
2020-03-17 22:13:57 +0530

    Updates

---
+++ precache-manifest.c5ddda1654949a2a69330c0ebf6374eb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65f025723e5e85aa141073391abb7d63",
+    "url": "/Movie-Star/index.html"
+  },
+  {
+    "revision": "98054f8e968a79cbb977",
+    "url": "/Movie-Star/static/css/2.27fdde94.chunk.css"
+  },
+  {
+    "revision": "f85108c6b42955d58fe5",
+    "url": "/Movie-Star/static/css/main.af749b73.chunk.css"
+  },
+  {
+    "revision": "98054f8e968a79cbb977",
+    "url": "/Movie-Star/static/js/2.a8f42ca2.chunk.js"
+  },
+  {
+    "revision": "38b6d0d0f8626b4800a346e0061c2296",
+    "url": "/Movie-Star/static/js/2.a8f42ca2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f85108c6b42955d58fe5",
+    "url": "/Movie-Star/static/js/main.ec1fc916.chunk.js"
+  },
+  {
+    "revision": "07b551870404064f974f",
+    "url": "/Movie-Star/static/js/runtime-main.34348655.js"
+  },
+  {
+    "revision": "f663ef002f70342105d9db6f12906741",
+    "url": "/Movie-Star/static/media/no_image.f663ef00.jpg"
+  },
+  {
+    "revision": "02a9430b88975cae16fcfcc9cf7b5799",
+    "url": "/Movie-Star/static/media/themovieorg.02a9430b.svg"
+  }
+]);
