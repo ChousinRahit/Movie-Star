f77721dadb8dbcc74f3fa9bbfdbb4b1a5d5dd723
ChousinRahit <2428kcr@gmail.com>
2020-03-18 14:45:17 +0530

    basename removed

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 const App = () => {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router>
       <div>
         <ScrollMemory />
         <Header />
